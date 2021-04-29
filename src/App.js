ebfa0b11c870351f6a5390ed9cdfcea167eabd58
Shuto Ueno <cubist.u@gmail.com>
2021-04-30 01:37:29 +0900

    use HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {HashRouter as Router, Route} from "react-router-dom";
 import About from "./About";
 import Game from "./Game";
 import Home from "./Home";
